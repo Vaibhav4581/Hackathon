02b8cba1ba5fbd3b2c57128ba3ab8800e1becf11
Vaibhav4581 <vaibhavkmoorthy.xb@gmail.com>
2026-03-10 14:58:29 +0530

    Initial commit - SmartFind React project

---
+++ smartfind/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>SmartFind - AI Lost & Found</h1>
+      <p>Report lost items and help others recover their belongings.</p>
+
+      <button>Report Lost Item</button>
+      <button>Report Found Item</button>
+    </div>
+  );
+}
+
+export default App;
